f17d0f539754e08041410f88472f0a21ecbad2b4
SuYxh <46957352+SuYxh@users.noreply.github.com>
2022-12-10 14:35:54 +0000

    Deploying to gh-pages from @ SuYxh/new-vitepress-demo@ef6347d4d704b326d714e4e9180737485133edf5 🚀

---
+++ assets/components_comA.md.ddebd495.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,d as p}from"./app.a95551f6.js";const m=JSON.parse('{"title":"ComA","description":"","frontmatter":{},"headers":[],"relativePath":"components/comA.md"}'),l={name:"components/comA.md"},o=p("",3),t=[o];function e(c,r,y,i,A,d){return n(),a("div",null,t)}const _=s(l,[["render",e]]);export{m as __pageData,_ as default};
